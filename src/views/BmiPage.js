91ac1e55aa3c6427e95426457cd199d981927fe5
alinuxD <alinux_xu@outlook.com>
2021-08-24 12:56:02 +1000

    bmi page

--- src/views/BmiPage.js
+++ src/views/BmiPage.js
@@ -4,26 +4,24 @@ import React from "react";
 import {
     Button,
     Input,
-    InputGroupAddon,
-    InputGroupText,
     InputGroup,
     Container,
     Row,
-    Col, CardBody, CardFooter, Card, Form, CardHeader,
+    Col, CardBody, CardFooter, Card,
 } from "reactstrap";
 
 // core components
 
 import DefaultFooter from "../components/Footers/DefaultFooter.js";
 import HomeNavbar from "../components/Navbars/HomeNavbar";
-import HomePageHeader from "../components/Headers/HomePageHeader";
-import IndexHeader from "../components/Headers/IndexHeader";
-import BmiPage from "./BmiPage";
 import BMIPageHeader from "../components/Headers/BMIPageHeader";
 
-function HomePage() {
-    const [firstFocus, setFirstFocus] = React.useState(false);
-    const [lastFocus, setLastFocus] = React.useState(false);
+function BmiPage() {
+    // const [firstFocus, setFirstFocus] = React.useState(false);
+    // const [lastFocus, setLastFocus] = React.useState(false);
+    const setFirstFocus = React.useState(false)[1]
+    const setLastFocus = React.useState(false)[1];
+
     React.useEffect(() => {
         document.body.classList.add("home-page");
         document.body.classList.add("sidebar-collapse");
@@ -174,75 +172,11 @@ function HomePage() {
                     </Container>
                 </div>
 
-                <div className="section section-contact-us text-center">
-                    <Container>
-                        <h2 className="title">Want to work with us?</h2>
-                        <p className="description">Your project is very important to us.</p>
-                        <Row>
-                            <Col className="text-center ml-auto mr-auto" lg="6" md="8">
-                                <InputGroup
-                                    className={
-                                        "input-lg" + (firstFocus ? " input-group-focus" : "")
-                                    }
-                                >
-                                    <InputGroupAddon addonType="prepend">
-                                        <InputGroupText>
-                                            <i className="now-ui-icons users_circle-08"></i>
-                                        </InputGroupText>
-                                    </InputGroupAddon>
-                                    <Input
-                                        placeholder="First Name..."
-                                        type="text"
-                                        onFocus={() => setFirstFocus(true)}
-                                        onBlur={() => setFirstFocus(false)}
-                                    ></Input>
-                                </InputGroup>
-                                <InputGroup
-                                    className={
-                                        "input-lg" + (lastFocus ? " input-group-focus" : "")
-                                    }
-                                >
-                                    <InputGroupAddon addonType="prepend">
-                                        <InputGroupText>
-                                            <i className="now-ui-icons ui-1_email-85"></i>
-                                        </InputGroupText>
-                                    </InputGroupAddon>
-                                    <Input
-                                        placeholder="Email..."
-                                        type="text"
-                                        onFocus={() => setLastFocus(true)}
-                                        onBlur={() => setLastFocus(false)}
-                                    ></Input>
-                                </InputGroup>
-                                <div className="textarea-container">
-                                    <Input
-                                        cols="80"
-                                        name="name"
-                                        placeholder="Type a message..."
-                                        rows="4"
-                                        type="textarea"
-                                    ></Input>
-                                </div>
-                                <div className="send-button">
-                                    <Button
-                                        block
-                                        className="btn-round"
-                                        color="info"
-                                        href="#pablo"
-                                        onClick={(e) => e.preventDefault()}
-                                        size="lg"
-                                    >
-                                        Send Message
-                                    </Button>
-                                </div>
-                            </Col>
-                        </Row>
-                    </Container>
-                </div>
+
                 <DefaultFooter />
             </div>
         </>
     );
 }
 
-export default HomePage;
+export default BmiPage;
